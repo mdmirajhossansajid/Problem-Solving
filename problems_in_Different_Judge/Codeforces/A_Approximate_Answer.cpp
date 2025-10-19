744500f39a48ce4a31ee2f1bc5c40daf3b0b7d2c
Miraj Hossan Sajid <mdmirajhossansajidpc@gmail.com>
2025-10-19 10:55:27 +0600

    Update and rename problems_in_Different_Judge/touriest to problems_in_Different_Judge/Codeforces/A_Approximate_Answer.cpp

---
+++ problems_in_Different_Judge/Codeforces/A_Approximate_Answer.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int x,y,z;cin>>x>>y>>z;
+   if(abs(x-y)<=z)
+   {
+    cout<<"Yes";
+   } 
+   else
+   cout<<"No";
+    return 0;
+}
