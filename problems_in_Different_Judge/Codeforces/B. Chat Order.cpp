8bebf8261b06e51f19d6ff55540b770c96037f96
Miraj Hossan Sajid <mdmirajhossansajidpc@gmail.com>
2025-11-02 14:31:06 +0600

    Add B. Chat Order solution in C++
    Implement solution for Codeforces problem B. Chat Order. using map

---
+++ problems_in_Different_Judge/Codeforces/B. Chat Order.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{   
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;cin>>n;
+    vector<string>v(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>v[i];
+    }
+    map<string,bool>mp;
+    for(int i=n-1;i>=0;i--)
+    {
+        if(!mp[v[i]])
+            cout<<v[i]<<endl;
+        mp[v[i]]=true;
+    }
+    return 0;
+}
