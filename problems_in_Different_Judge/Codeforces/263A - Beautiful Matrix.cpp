ae9807a0bee3ef4bf9caa730b968f833d6856bed
Miraj Hossan Sajid <mdmirajhossansajidpc@gmail.com>
2025-11-09 23:37:51 +0600

    Add solution for Beautiful Matrix problem
    Implement solution for Codeforces problem 263A.

---
+++ problems_in_Different_Judge/Codeforces/263A - Beautiful Matrix.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int r = 0, c = 0;
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= 5; j++) {
+            int x;
+            cin >> x;
+            if (x == 1) {
+                r = i;
+                c = j;
+            }
+        }
+    }
+    cout << abs(r - 3) + abs(c - 3) << "\n";
+    return 0;
+}
